783a2d5612d07f404db5bfa19e4424e96b04c76f
Kshamith Paidiwar <2840861P@student.gla.ac.uk>
2024-02-01 10:33:20 +0000

    Adds 4.3 changes

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -56,6 +56,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 
 TEMPLATES = [
     {
@@ -68,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -127,3 +130,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
