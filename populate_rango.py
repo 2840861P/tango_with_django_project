2a9ee06505457da30a096a98c614d1fa81c3a8aa
Kshamith Paidiwar <2840861P@student.gla.ac.uk>
2024-02-03 11:59:38 +0000

    Updates till 5th chapter

--- populate_rango.py
+++ populate_rango.py
@@ -27,12 +27,12 @@ def populate():
          'url':'http://bottlepy.org/docs/dev/'},
         {'title':'Flask',
          'url':'http://flask.pocoo.org'} ]
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'likes': 64, 'views': 128},
+            'Django': {'pages': django_pages, 'likes': 32, 'views': 64}, 
+            'Other Frameworks': {'pages': other_pages, 'likes': 16, 'views':32} }
     
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat,cat_data['likes'],cat_data['views'])
         for p in cat_data['pages']:
                     add_page(c, p['title'], p['url'])
             # Print out the categories we have added.
@@ -47,8 +47,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0] 
+def add_cat(name,likes = 0,views = 0):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.likes=likes
+    c.views=views 
     c.save()
     return c
     
