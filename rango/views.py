0762c50069099403970c708bdf33b5ae1a16f5a4
2840861P <2840861P@student.gla.ac.uk>
2025-02-14 12:27:35 +0000

    updates user login and logout

--- rango/views.py
+++ rango/views.py
@@ -99,7 +99,23 @@ def register(request):
     
     return render(request, 'rango/register.html', {'form': form})
 
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data.get('username'),
+                                password=form.cleaned_data.get('password'))
+            if user:
+                login(request, user)
+                return redirect('rango:index')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'rango/login.html', {'form': form})
 
+def user_logout(request):
+    logout(request)
+    return redirect('rango:index')
 
 
 
